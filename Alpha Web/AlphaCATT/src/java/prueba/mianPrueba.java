91555463f3ae29f16ced4f871e37c7071c686bc2
hazziely <hbernachef1100@alumno.ipn.mx>
2017-02-06 18:13:14 -0600

    intento de meter a los profes, pedos con de la O y con los horarios =/

--- Alpha Web/AlphaCATT/src/java/prueba/mianPrueba.java
+++ Alpha Web/AlphaCATT/src/java/prueba/mianPrueba.java
@@ -5,9 +5,16 @@
  */
 package prueba;
 
+import com.myapp.bs.ProfesorBs;
 import com.myapp.bs.SalaBs;
+import com.myapp.model.Horario;
+import com.myapp.model.Profesor;
 import java.util.List;
 import com.myapp.model.Sala;
+import com.myapp.modulo.LeerExcel;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Arrays;
 
 /**
  *
@@ -24,27 +31,45 @@ public class mianPrueba {
 //        {
 //            System.out.println("id: " + salas.get(i).getIdSala() + " Nombre: " + salas.get(i).getNombre());
 //        }
+//        
+//        List<Sala> salas;
+//        
+//        Sala s = new Sala("nueva sala");
+//        for(int i = 18; i < 53; i++)
+//        {
+//            if(i == 24)continue;
+//            s.setIdSala(i);
+//            SalaBs.delete(s);
+//        }
+//        s.setIdSala(56);
+//        SalaBs.delete(s);
+//        s.setIdSala(57);
+//        SalaBs.delete(s);
+//        s.setIdSala(58);
+//        SalaBs.delete(s);
+//        salas = SalaBs.findAll();
+//        for(int i = 0; i < salas.size(); i++)
+//        {
+//            System.out.println("id: " + salas.get(i).getIdSala() + " Nombre: " + salas.get(i).getNombre());
+//        }
         
-        List<Sala> salas;
+        FileInputStream file = new FileInputStream(new File("../Estructura 2017 1.xls"));
+        List<Profesor> profesores = LeerExcel.ExtraeExcel(file);
+//        for(int j = 0;j<profesores.size();j++)
+//        {
+//            //profesores.get(j).setIdProfesor(j+1);
+//            System.out.println("id: " + profesores.get(j).getIdProfesor() + ": " + profesores.get(j).getApaterno() + ", "+ profesores.get(j).getAmaterno() + ", " + profesores.get(j).getNombre());
+//            List<Horario> horarios = Arrays.asList(profesores.get(j).getHorarios().toArray(new Horario[0]));
+//            for(int i = 0; i < horarios.size(); i++)
+//            {
+//                Horario hor = horarios.get(i);
+//                System.out.println("\tHorario: " + hor.getLun() + " " + hor.getMar() + " " + hor.getMie() + " " + hor.getJue() + " " + hor.getVie() );
+//            }
+//        }
+        for(int i = 0; i < profesores.size(); i++)
+        {
+            ProfesorBs.save(profesores.get(i));
+        }
         
-        Sala s = new Sala("nueva sala");
-        for(int i = 18; i < 53; i++)
-        {
-            if(i == 24)continue;
-            s.setIdSala(i);
-            SalaBs.delete(s);
-        }
-        s.setIdSala(56);
-        SalaBs.delete(s);
-        s.setIdSala(57);
-        SalaBs.delete(s);
-        s.setIdSala(58);
-        SalaBs.delete(s);
-        salas = SalaBs.findAll();
-        for(int i = 0; i < salas.size(); i++)
-        {
-            System.out.println("id: " + salas.get(i).getIdSala() + " Nombre: " + salas.get(i).getNombre());
-        }
-    }
-    
+    }    
 }
