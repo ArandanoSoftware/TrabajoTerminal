cf4cfc973aba603b2eed2547d1e24d4e1623c5c9
Roberto Valdez <robonline7@gmail.com>
2017-03-10 14:43:51 +0000

    Solución de Errores JS

--- Alpha Web/AlphaCATT/web/js/master.js
+++ Alpha Web/AlphaCATT/web/js/master.js
@@ -1,3 +1,24 @@
+// TestGreeting 
+function greeting(){
+  var today= new Date();
+  var hournow= today.getHours();
+  var greeting;
+
+  if (hournow > 18){
+    greeting= 'Buenas Noches';
+  }else if (hournow > 12) {
+    greeting= 'Buenas Tardes';
+  }else if (hournow > 0) {
+    greeting= 'Buenos Días';
+  }else {
+    greeting= 'Bienvenido';
+  }
+
+  document.write('<h1>'+ greeting +'</h1>');
+}
+
+
+
 // Disable  Checkbox
 function enableDisable(bEnable, textBoxID)
    {
